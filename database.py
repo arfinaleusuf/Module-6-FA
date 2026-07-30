1381e8ce93340382b930ee07649dfb4ed55cc93c
arfinaleusuf <arfinaleusuf@gmail.com>
2026-07-30 23:24:20 +0600

    6.1 complete

--- database.py
+++ database.py
@@ -7,3 +7,5 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
 
 SessionLocal = sessionmaker(autoflush=False, autocomit=False, bind=engine)
+
+Base = declarative_base()
