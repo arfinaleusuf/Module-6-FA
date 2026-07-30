13b5646a25e03073163499ac28480da78775b49f
arfinaleusuf <arfinaleusuf@gmail.com>
2026-07-31 01:56:30 +0600

    get all from database

--- database.py
+++ database.py
@@ -6,6 +6,6 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
 
-SessionLocal = sessionmaker(autoflush=False, autocomit=False, bind=engine)
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
